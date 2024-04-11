92e94f8af767d2aa93fc1bdbe6f1cb3d9e94b99d
metaproph3t <metaproph3t@protonmail.com>
2024-04-11 00:00:00 +0000

    Add `swap` and `provide_liquidity`

--- programs/amm/src/lib.rs
+++ programs/amm/src/lib.rs
@@ -11,6 +11,13 @@ pub struct Pool {
     pub token_1: Pubkey,
     pub token_0_reserves: u64,
     pub token_1_reserves: u64,
+    pub pda_bump: u8,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, PartialEq, Eq)]
+pub enum SwapDirection {
+    Token0ToToken1,
+    Token1ToToken0,
 }
 
 #[program]
@@ -23,7 +30,124 @@ pub mod amm {
             token_1: ctx.accounts.token_1.key(),
             token_0_reserves: 0,
             token_1_reserves: 0,
+            pda_bump: ctx.bumps.pool,
         });
+
+        Ok(())
+    }
+
+    pub fn provide_liquidity(
+        ctx: Context<ProvideLiquidity>,
+        token_0_max: u64,
+        token_1_max: u64,
+    ) -> Result<()> {
+        let pool = &mut ctx.accounts.pool;
+
+        if pool.token_0_reserves == 0 {
+            // if the pool doesn't currently have liquidity, transfer tokens in the provided proportion
+
+            for (user_token_account, token_vault, amount) in [
+                (
+                    &ctx.accounts.user_token_0_account,
+                    &ctx.accounts.token_0_vault,
+                    token_0_max,
+                ),
+                (
+                    &ctx.accounts.user_token_1_account,
+                    &ctx.accounts.token_1_vault,
+                    token_1_max,
+                ),
+            ] {
+                let cpi_ctx = CpiContext::new(
+                    ctx.accounts.token_program.to_account_info(),
+                    token::Transfer {
+                        from: user_token_account.to_account_info(),
+                        to: token_vault.to_account_info(),
+                        authority: ctx.accounts.signer.to_account_info(),
+                    },
+                );
+                token::transfer(cpi_ctx, amount)?;
+            }
+
+            pool.token_0_reserves = token_0_max;
+            pool.token_1_reserves = token_1_max;
+        }
+
+        Ok(())
+    }
+
+    pub fn swap(ctx: Context<Swap>, direction: SwapDirection, amount_in: u64) -> Result<()> {
+        let pool = &mut ctx.accounts.pool;
+
+        let (user_from, user_to, vault_to, vault_from) = match direction {
+            SwapDirection::Token0ToToken1 => (
+                &ctx.accounts.user_token_0_account,
+                &ctx.accounts.user_token_1_account,
+                &ctx.accounts.token_0_vault,
+                &ctx.accounts.token_1_vault,
+            ),
+            SwapDirection::Token1ToToken0 => (
+                &ctx.accounts.user_token_1_account,
+                &ctx.accounts.user_token_0_account,
+                &ctx.accounts.token_1_vault,
+                &ctx.accounts.token_0_vault,
+            ),
+        };
+
+        let cpi_ctx = CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            token::Transfer {
+                from: user_from.to_account_info(),
+                to: vault_to.to_account_info(),
+                authority: ctx.accounts.signer.to_account_info(),
+            },
+        );
+
+        token::transfer(cpi_ctx, amount_in)?;
+
+        // x * y = k
+        // (x + Δx)(y - Δy) = k
+        // FROM THIS WE CAN DERIVE
+        // (y - Δy) = k / (x + Δx)
+        // -Δy = [k / (x + Δx)] - y
+        // Δy = -[k / (x + Δx)] + y
+        // Δy = y - [k / (x + Δx)]
+        // WE CAN ALSO DERIVE
+        // (x + Δx) = k / (y - Δy)
+        // Δx = [k / y - Δy] - x
+        
+        let x = u128::from(pool.token_0_reserves);
+        let y = u128::from(pool.token_1_reserves);
+        let k = x * y;
+        
+        let amount_out = if direction == SwapDirection::Token0ToToken1 {
+            let delta_x = u128::from(amount_in);
+            let delta_y = y - (k / (x + delta_x));
+
+            u64::try_from(delta_y).unwrap()
+        } else {
+            let delta_y = u128::from(amount_in);
+            let delta_x = (k / (y + delta_y)) - x;
+
+            u64::try_from(delta_x).unwrap()
+        };
+
+        let token_0 = pool.token_0.key();
+        let token_1 = pool.token_1.key();
+        let seeds = &[b"pool", token_0.as_ref(), token_1.as_ref(), &[pool.pda_bump]];
+        let signer = &[&seeds[..]];
+        
+        let cpi_ctx = CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            token::Transfer {
+                from: vault_from.to_account_info(),
+                to: user_to.to_account_info(),
+                authority: ctx.accounts.pool.to_account_info(),
+            },
+            signer
+        );
+
+        token::transfer(cpi_ctx, amount_out)?;
 
         Ok(())
     }
@@ -33,6 +157,8 @@ pub mod amm {
 pub struct InitializePool<'info> {
     #[account(
         init,
+        seeds = [b"pool", token_0.key().as_ref(), token_1.key().as_ref()],
+        bump,
         space = 8 + std::mem::size_of::<Pool>(),
         payer = payer,
     )]
@@ -45,6 +171,54 @@ pub struct InitializePool<'info> {
     )]
     pub token_0: Account<'info, token::Mint>,
     pub token_1: Account<'info, token::Mint>,
+}
+
+#[derive(Accounts)]
+pub struct ProvideLiquidity<'info> {
+    #[account(mut)]
+    pub pool: Account<'info, Pool>,
+    #[account(mut, token::mint = pool.token_0)]
+    pub user_token_0_account: Account<'info, token::TokenAccount>,
+    #[account(mut, token::mint = pool.token_1)]
+    pub user_token_1_account: Account<'info, token::TokenAccount>,
+    pub signer: Signer<'info>,
+    #[account(
+        mut,
+        associated_token::mint = pool.token_0,
+        associated_token::authority = pool
+    )]
+    pub token_0_vault: Account<'info, token::TokenAccount>,
+    #[account(
+        mut,
+        associated_token::mint = pool.token_1,
+        associated_token::authority = pool
+    )]
+    pub token_1_vault: Account<'info, token::TokenAccount>,
+    pub token_program: Program<'info, token::Token>,
+}
+
+#[derive(Accounts)]
+pub struct Swap<'info> {
+    pub signer: Signer<'info>,
+    #[account(mut)]
+    pub pool: Account<'info, Pool>,
+    #[account(mut, token::mint = pool.token_0)]
+    pub user_token_0_account: Account<'info, token::TokenAccount>,
+    #[account(mut, token::mint = pool.token_1)]
+    pub user_token_1_account: Account<'info, token::TokenAccount>,
+    #[account(
+        mut,
+        associated_token::mint = pool.token_0,
+        associated_token::authority = pool
+    )]
+    pub token_0_vault: Account<'info, token::TokenAccount>,
+    #[account(
+        mut,
+        associated_token::mint = pool.token_1,
+        associated_token::authority = pool
+    )]
+    pub token_1_vault: Account<'info, token::TokenAccount>,
+    pub token_program: Program<'info, token::Token>,
 }
 
 #[error_code]
--- tests/amm.ts
+++ tests/amm.ts
@@ -2,15 +2,17 @@ import * as token from "@solana/spl-token";
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Amm } from "../target/types/amm";
+import { BN } from "bn.js";
+import { assert } from "chai";
 
 describe("amm", async () => {
   const provider = anchor.AnchorProvider.env();
   const connection = provider.connection;
-  const payer = provider.wallet['payer'];
+  const payer = provider.wallet["payer"];
   anchor.setProvider(provider);
   const amm = anchor.workspace.Amm as Program<Amm>;
 
-  let token0, token1;
+  let token0, token1, userToken0Account, userToken1Account;
 
   before(async () => {
     token0 = await token.createMint(
@@ -28,18 +30,125 @@ describe("amm", async () => {
       payer.publicKey,
       9
     );
+
+    userToken0Account = await token.createAssociatedTokenAccount(
+      connection,
+      payer,
+      token0,
+      payer.publicKey
+    );
+
+    userToken1Account = await token.createAssociatedTokenAccount(
+      connection,
+      payer,
+      token1,
+      payer.publicKey
+    );
+
+    await token.mintTo(
+      connection,
+      payer,
+      token0,
+      userToken0Account,
+      payer,
+      1_000 * 1_000_000_000
+    );
+
+    await token.mintTo(
+      connection,
+      payer,
+      token1,
+      userToken1Account,
+      payer,
+      1_000 * 1_000_000_000
+    );
   });
 
-  it("Can initialize pools", async () => {
-    const poolKP = anchor.web3.Keypair.generate();
+  it("Works", async () => {
+    const [pool] = anchor.web3.PublicKey.findProgramAddressSync([
+      anchor.utils.bytes.utf8.encode("pool"),
+      token0.toBuffer(),
+      token1.toBuffer(),
+    ], amm.programId);
 
-    await amm.methods.initializePool()
+    await amm.methods
+      .initializePool()
       .accounts({
-        pool: poolKP.publicKey,
+        pool,
         token0,
         token1,
       })
-      .signers([poolKP])
       .rpc();
+
+    const token0Vault = (await token.getOrCreateAssociatedTokenAccount(
+      connection,
+      payer,
+      token0,
+      pool,
+      true
+    )).address;
+
+    const token1Vault = (await token.getOrCreateAssociatedTokenAccount(
+      connection,
+      payer,
+      token1,
+      pool,
+      true
+    )).address;
+
+    await amm.methods
+      .provideLiquidity(new BN(25), new BN(10))
+      .accounts({
+        pool,
+        userToken0Account,
+        userToken1Account,
+        token0Vault,
+        token1Vault,
+      })
+      .rpc();
+
+    const storedPool = await amm.account.pool.fetch(pool);
+
+    assert.ok(storedPool.token0Reserves.eqn(25));
+    assert.ok(storedPool.token1Reserves.eqn(10));
+
+    const preToken0Balance = (
+      await token.getAccount(connection, userToken0Account)
+    ).amount;
+    const preToken1Balance = (
+      await token.getAccount(connection, userToken1Account)
+    ).amount;
+
+    // 25 * 10 = 250, so k = 250
+    // if we add 25 to x, 50 * y = 250 or y = 250 / 50 or y = 5
+    // 10 - 5 = 5, so we should get 5 out of the pool
+    await amm.methods
+      .swap({ token0ToToken1: {} }, new BN(25))
+      .accounts({
+        pool,
+        userToken0Account,
+        userToken1Account,
+        token0Vault,
+        token1Vault,
+      })
+      .rpc();
+
+    const postToken0Balance = (
+      await token.getAccount(connection, userToken0Account)
+    ).amount;
+    const postToken1Balance = (
+      await token.getAccount(connection, userToken1Account)
+    ).amount;
+
+    assert.equal(
+      postToken0Balance,
+      preToken0Balance - BigInt(25),
+      "user sent wrong amount"
+    );
+    assert.equal(
+      postToken1Balance,
+      preToken1Balance + BigInt(5),
+      "user received wrong amount"
+    );
   });
 });
